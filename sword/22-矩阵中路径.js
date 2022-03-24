18abcc1e02bee37953ccbaaa2753872d21c161b2
tongzijie <tongzijie@accesscorporate.com.cn>
2022-03-24 18:26:21 +0800

    sword-day14

---
+++ sword/22-矩阵中路径.js
@@ -0,0 +1,17 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ * 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。
+ * 如果 word 存在于网格中，返回 true ；否则，返回 false 。
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+ * 同一个单元格内的字母不允许被重复使用。
+ */
+var exist = function (board, word) {
+  let row = 0,
+    col = 0,
+    i = 0;
+  const n = board[0].length;
+  const m = board.length;
+  while (row < n && col < m) {}
+};
