0ac7dc6101d3349d0ef6853935a61f74178942c1
2451499480@qq.com <2451499480@qq.com>
2022-03-25 00:09:43 +0800

    sword day14

--- sword/22-矩阵中路径.js
+++ sword/22-矩阵中路径.js
@@ -8,10 +8,63 @@
  * 同一个单元格内的字母不允许被重复使用。
  */
 var exist = function (board, word) {
-  let row = 0,
-    col = 0,
-    i = 0;
-  const n = board[0].length;
-  const m = board.length;
-  while (row < n && col < m) {}
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] === word.charAt(0)) {
+        if (dfs(board, word, [i, j])) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 };
+function dfs(board, word, start) {
+  if (word.length === 1) {
+    return true;
+  }
+  let [i, j] = start;
+  if (i > 0 && board[i - 1][j] === word.charAt(1)) {
+    board[i][j] = "0";
+    if (dfs(board, word.substring(1), [i - 1, j])) {
+      return true;
+    } else {
+      board[i][j] = word.charAt(0);
+    }
+  }
+  if (j > 0 && board[i][j - 1] === word.charAt(1)) {
+    board[i][j] = "0";
+    if (dfs(board, word.substring(1), [i, j - 1])) {
+      return true;
+    } else {
+      board[i][j] = word.charAt(0);
+    }
+  }
+  if (i < board.length - 1 && board[i + 1][j] === word.charAt(1)) {
+    board[i][j] = "0";
+    if (dfs(board, word.substring(1), [i + 1, j])) {
+      return true;
+    } else {
+      board[i][j] = word.charAt(0);
+    }
+  }
+  if (j < board[0].length - 1 && board[i][j + 1] === word.charAt(1)) {
+    board[i][j] = "0";
+    if (dfs(board, word.substring(1), [i, j + 1])) {
+      return true;
+    } else {
+      board[i][j] = word.charAt(0);
+    }
+  }
+  return false;
+}
+console.log(
+  exist(
+    [
+      ["A", "B", "C", "E"],
+      ["S", "F", "C", "S"],
+      ["A", "D", "E", "E"],
+    ],
+    "ABCCED"
+  )
+);
