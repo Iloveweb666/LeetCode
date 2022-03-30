b4864f0e7b19b4721ddb114fd1a8ab5e69de090c
tongzijie <tongzijie@accesscorporate.com.cn>
2022-03-30 17:09:21 +0800

    sword-day15

--- sword/23-机器人的运动范围.js
+++ sword/23-机器人的运动范围.js
@@ -10,25 +10,34 @@
  * 请问该机器人能够到达多少个格子？
  */
 var movingCount = function (m, n, k) {
+  // 允许走过的节点数（可以用visited.size()替代）
   let count = 0;
+  // 走过的节点
   let visited = new Set(['0,0']);
+  // 需要确定的节点队列（DFS处理）
   let queue = [[0, 0]];
+  // 循环结束条件：没有待确认的节点队列
   while (queue.length) {
+    // 当前节点
     const current = queue.shift();
+    // 当前节点符合要求
     if (getSum(current, k)) {
+      // 计数+1
       count++;
+      // 非最后一行时 把当前节点下面推入队列
       if (current[1] < m - 1) {
-        const bottom = [current[0], current[1] + 1].toString();
-        if (!visited.has(bottom)) {
-          visited.add(bottom);
-          queue.push([current[0], current[1] + 1]);
+        const bottom = [current[0], current[1] + 1];
+        if (!visited.has(bottom.toString())) {
+          visited.add(bottom.toString());
+          queue.push(bottom);
         }
       }
+      // 非最后一列时 把当前节点右边推入队列
       if (current[0] < n - 1) {
-        const right = [current[0] + 1, current[1]].toString();
-        if (!visited.has(right)) {
-          visited.add(right);
-          queue.push([current[0] + 1, current[1]]);
+        const right = [current[0] + 1, current[1]];
+        if (!visited.has(right.toString())) {
+          visited.add(right.toString());
+          queue.push(right);
         }
       }
     }
@@ -36,11 +45,13 @@ var movingCount = function (m, n, k) {
   return count;
 };
 
+// 获取数组数位和是否小于等于k
 var getSum = function (current, k) {
   const sum = numSum(current[0]) + numSum(current[1]);
   return sum <= k;
 };
 
+// 获取单个数字数位和
 var numSum = function (num) {
   let s = 0;
   while (num != 0) {
