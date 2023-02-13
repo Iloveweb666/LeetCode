a0aeb1ed5efb1da7a0f35375245d2d7373082727
tongzijie <tongzijie@accesscorporate.com.cn>
2023-02-13 18:07:56 +0800

    56

--- 56.合并区间.js
+++ 56.合并区间.js
@@ -17,29 +17,17 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-  const step = 1;
-  while (step < intervals.length) {
-    for (let i = 0; i < intervals.length; i += 1) {
-      const i1 = i;
-      const i2 = i + step;
-      if (i2 >= intervals.length) {
-        step += 1;
-        break;
-      } // i2越界结束循环
-      // 取两个值，保证升序
-      const arr1 = intervals[i1][0] < intervals[i2][0] ? intervals[i1] : intervals[i2];
-      const arr2 = intervals[i1][0] < intervals[i2][0] ? intervals[i2] : intervals[i1];
-      const newArr = [];
-      if (arr1[1] < arr2[0]) {
-        // 无交集 下次循环
-        step += 1;
-        continue;
-      } else {
-        // 合并区间
-        newArr.push(arr1[0], arr2[1]);
-        //  修改intervals - i1位置替换为newArr，i2位置删除
-      }
+  if (!intervals || intervals.length < 2) return intervals;
+  intervals.sort((a, b) => a[0] - b[0]);
+  let res = [intervals[0]];
+  let len = intervals.length;
+  for (let i = 1; i < len; i++) {
+    if (intervals[i][0] <= res[res.length - 1][1]) {
+      res[res.length - 1][1] = Math.max(res[res.length - 1][1], intervals[i][1]);
+    } else {
+      res.push(intervals[i]);
     }
   }
+  return res;
 };
 // @lc code=end
